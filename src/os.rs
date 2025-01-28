e9d29fd5a700622d574fef931b5d6a427f07da4b
Alex Kotusenko <alexkotusenko@gmail.com>
2025-01-28 16:18:39 -0500

    added new functionality

--- src/os.rs
+++ src/os.rs
@@ -6,3 +6,53 @@ pub fn unless_unix<T: Fn() -> ()>(fallback: T) {
         fallback();
     }
 }
+
+pub fn is_unix() -> bool {
+    let os = std::env::consts::OS;
+    if os == "macos" || os == "linux" {
+        return true;
+    }
+    false
+}
+
+pub fn get_unix_home_folder() -> Result<String, String> {
+    if !is_unix() {
+        return Err("The OS is not Unix-based".to_string());
+    }
+    match std::env::var("HOME") {
+        Ok(path) => {
+            return Ok(path); 
+        }
+        Err(_) => {
+            return Err("Failed to determine the home directory.".to_string());
+        }
+    }
+}
+
+pub fn get_unix_user() -> Result<String, String> {
+    if !is_unix() {
+        return Err("The OS is not Unix-based".to_string());
+    }
+    match std::env::var("USER") {
+        Ok(user) => {
+            return Ok(user); 
+        }
+        Err(_) => {
+            return Err("Failed to determine the current user.".to_string());
+        }
+    }
+}
+
+pub fn get_unix_shell() -> Result<String, String> {
+    if !is_unix() {
+        return Err("The OS is not Unix-based".to_string());
+    }
+    match std::env::var("SHELL") {
+        Ok(user) => {
+            return Ok(user); 
+        }
+        Err(_) => {
+            return Err("Failed to determine the shell used.".to_string());
+        }
+    }
+}
